621afe828a10208753b12bec619c845f83e1b218
Henry Le <henryle10@gmail.com>
2023-04-11 22:19:42 -0700

    remove overlay

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ const App = () => {
     <BrowserRouter>
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <div className="w-full h-full flex justify-center items-center backdrop-brightness-50">
           <Navbar />
           <Hero />
-        </div>
       </div>
         <About />
         <Experience />
