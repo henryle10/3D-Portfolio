e35c2c5ae452a425b622cad92c55906b863106fa
Henry Le <henryle10@gmail.com>
2023-04-11 22:16:52 -0700

    tweeked navbar

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,7 @@
 import Tilt from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { github, live } from '../assets';
+import { live } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
